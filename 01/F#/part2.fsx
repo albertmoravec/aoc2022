db2cc7e61df5771e591e027d39226190bc482fbb
Albert Moravec <albert.moravec@gmail.com>
2022-12-01 11:23:52 +0100

    Implemented Day 1, part 2 in F#

---
+++ 01/F#/part2.fsx
@@ -0,0 +1,14 @@
+﻿open System.IO
+
+let rec collectItems acc item = match (acc, item) with
+                                | (acc, "") -> [] :: acc
+                                | (head :: tail, item) -> (int item :: head) :: tail
+                                | ([], item) -> [[int item]]
+
+File.ReadLines("./01/input.txt")
+|> List.ofSeq
+|> List.fold(collectItems) []
+|> List.map(List.sum)
+|> List.sortDescending
+|> List.take 3
+|> List.sum
