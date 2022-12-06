16c007cfadf6adeb9d9ad8e67330ab292c7e6ca3
Albert Moravec <albert.moravec@gmail.com>
2022-12-06 11:21:26 +0100

    Implement day 2 in F#

---
+++ 02/F#/part1.fsx
@@ -0,0 +1,46 @@
+﻿open System.IO
+
+type Choice =
+    | Rock
+    | Paper
+    | Scissors
+    
+type Outcome =
+    | Win
+    | Draw
+    | Loss
+
+let mapChoice choice = match choice with
+                       | "A" | "X" -> Rock
+                       | "B" | "Y" -> Paper
+                       | "C" | "Z" -> Scissors
+                       | _ -> failwith "Unknown choice"
+                       
+let outcome opponentsChoice myChoice = match (myChoice, opponentsChoice) with                                       
+                                       | Rock, Paper -> Loss
+                                       | Rock, Scissors -> Win
+                                       | Paper, Rock -> Win
+                                       | Paper, Scissors -> Loss
+                                       | Scissors, Rock -> Loss
+                                       | Scissors, Paper -> Win
+                                       | _, _ -> Draw
+
+let outcomePoints outcome = match outcome with
+                            | Win -> 6
+                            | Draw -> 3
+                            | Loss -> 0
+
+let choicePoints choice = match choice with
+                          | Rock -> 1
+                          | Paper -> 2
+                          | Scissors -> 3
+                          
+let mapTuple f (a, b) = (f a, f b)
+    
+File.ReadLines("./02/input.txt")
+|> List.ofSeq
+|> List.map(fun x -> x.Split(" "))
+|> List.map(fun x -> (x.[0], x.[1]))
+|> List.map(mapTuple mapChoice)
+|> List.map(fun (x, y) -> (outcomePoints (outcome x y)) + (choicePoints y))
+|> List.sum
---
+++ 02/F#/part2.fsx
@@ -0,0 +1,65 @@
+﻿open System.IO
+
+type Choice =
+    | Rock
+    | Paper
+    | Scissors
+    
+type Outcome =
+    | Win
+    | Draw
+    | Loss
+
+let mapChoice choice =
+    match choice with
+    | "A" -> Rock
+    | "B" -> Paper
+    | "C" -> Scissors
+    | _ -> failwith "Unknown choice"
+                       
+let mapOutcome outcome =
+    match outcome with
+    | "X" -> Loss
+    | "Y" -> Draw
+    | "Z" -> Win
+    | _ -> failwith "Unknown outcome"
+                         
+let myChoice opponentChoice outcome =
+    match (opponentChoice, outcome) with
+    | c, Draw -> c
+    | Rock, Win -> Paper
+    | Rock, Loss -> Scissors
+    | Paper, Win -> Scissors
+    | Paper, Loss -> Rock
+    | Scissors, Win -> Rock
+    | Scissors, Loss -> Paper
+                       
+let outcome opponentsChoice myChoice =
+    match (myChoice, opponentsChoice) with
+    | Rock, Paper -> Loss
+    | Rock, Scissors -> Win
+    | Paper, Rock -> Win
+    | Paper, Scissors -> Loss
+    | Scissors, Rock -> Loss
+    | Scissors, Paper -> Win
+    | _, _ -> Draw
+
+let outcomePoints outcome =
+    match outcome with
+    | Win -> 6
+    | Draw -> 3
+    | Loss -> 0
+
+let choicePoints choice =
+    match choice with
+    | Rock -> 1
+    | Paper -> 2
+    | Scissors -> 3
+                              
+File.ReadLines("./02/input.txt")
+|> List.ofSeq
+|> List.map(fun x -> x.Split(" "))
+|> List.map(fun x -> (mapChoice x.[0], mapOutcome x.[1]))
+|> List.map(fun (opponentChoice, outcome) -> (opponentChoice, myChoice opponentChoice outcome))
+|> List.map(fun (x, y) -> (outcomePoints (outcome x y)) + (choicePoints y))
+|> List.sum
