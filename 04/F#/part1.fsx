61ee731b26411f7af6fdbc1e9a7b1c6873ae0bbf
Albert Moravec <albert.moravec@gmail.com>
2022-12-09 19:53:34 +0100

    Implemented Day 4 in F#

---
+++ 04/F#/part1.fsx
@@ -0,0 +1,30 @@
+﻿open System.IO
+
+let rec pair list = (List.item 0 list, List.item 1 list)
+
+let parseRange (range: string) =
+    range.Split("-")
+    |> List.ofArray
+    |> List.map int
+
+let rec parseRanges (ranges: string) =
+    ranges.Split(",")
+    |> List.ofArray
+    |> List.map parseRange
+    |> List.map pair
+    
+let numberRange (low, high) = [ low .. high ]
+
+let contained (rangeOne, rangeTwo) =
+    Set.isSuperset rangeOne rangeTwo || Set.isSuperset rangeTwo rangeOne
+
+
+File.ReadLines("./04/input.txt")
+|> List.ofSeq
+|> List.map parseRanges
+|> List.map (List.map numberRange)
+|> List.map (List.map Set.ofList)
+|> List.map pair
+|> List.map contained
+|> List.where id
+|> List.length
---
+++ 04/F#/part2.fsx
@@ -0,0 +1,32 @@
+﻿open System.IO
+
+let rec pair list = (List.item 0 list, List.item 1 list)
+
+let parseRange (range: string) =
+    range.Split("-")
+    |> List.ofArray
+    |> List.map int
+
+let rec parseRanges (ranges: string) =
+    ranges.Split(",")
+    |> List.ofArray
+    |> List.map parseRange
+    |> List.map pair
+    
+let numberRange (low, high) = [ low .. high ]
+
+let intersects (rangeOne, rangeTwo) =
+    Set.intersect rangeOne rangeTwo
+    |> Set.isEmpty
+    |> not
+
+
+File.ReadLines("./04/input.txt")
+|> List.ofSeq
+|> List.map parseRanges
+|> List.map (List.map numberRange)
+|> List.map (List.map Set.ofList)
+|> List.map pair
+|> List.map intersects
+|> List.where id
+|> List.length
