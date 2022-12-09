3651b37d89560ba2649b053a082dc2b150f76cfd
Albert Moravec <albert.moravec@gmail.com>
2022-12-09 19:12:51 +0100

    Implemented Day 3 in F#

---
+++ 03/F#/part1.fsx
@@ -0,0 +1,23 @@
+﻿open System.IO
+
+let rec splitCompartments content =
+    content
+    |> Seq.toList
+    |> List.splitInto 2
+    |> List.map Set.ofList
+    
+let rec findDuplicate compartments =
+    Set.intersectMany compartments
+    |> Set.minElement
+    
+let itemToPriority item =
+    match item with
+    | item when item >= 'a' && item <= 'z' -> int item - int 'a' + 1
+    | item when item >= 'A' && item <= 'Z' -> int item - int 'A' + 27    
+    
+File.ReadLines("./03/input.txt")
+|> List.ofSeq
+|> List.map splitCompartments
+|> List.map findDuplicate
+|> List.map itemToPriority
+|> List.sum
---
+++ 03/F#/part2.fsx
@@ -0,0 +1,26 @@
+﻿open System.IO
+
+let rec splitCompartments content =
+    content
+    |> Seq.toList
+    |> List.splitInto 2
+    |> fun list -> (list.[0], list.[1])
+    
+let rec findTriplicate rucksacks =
+    rucksacks
+    |> List.map Set.ofList
+    |> Set.intersectMany
+    |> Set.minElement
+    
+let itemToPriority item =
+    match item with
+    | item when item >= 'a' && item <= 'z' -> int item - int 'a' + 1
+    | item when item >= 'A' && item <= 'Z' -> int item - int 'A' + 27
+    
+File.ReadLines("./03/input.txt")
+|> List.ofSeq
+|> List.map List.ofSeq
+|> List.chunkBySize 3
+|> List.map findTriplicate
+|> List.map itemToPriority
+|> List.sum
